675961a637234c9132545b63f8eea9793c6fbea6
Wake <georgerincon2@gmail.com>
2018-11-07 19:36:48 -0500

    added jump animation on key down, started on run&jump animation, added attack animations to player sprite

--- resources/js/state1.js
+++ resources/js/state1.js
@@ -1,15 +1,15 @@
 let demo = window.demo || (window.demo = {});
 
 let pSpeed = 15;
-var player,
+let player,
     platform,
-    sprite2;
+    isAttack;
 
 demo.state1 = function () { };
 demo.state1.prototype = {
     preload: function () {
         //preloads spritesheets to be used in create
-        game.load.spritesheet('tester', 'resources/art/test-scott-spritesheet.png', 165, 186, 16);
+        game.load.spritesheet('tester', 'resources/art/test-scott-run-attack-combo.png', 213, 204, 63);
         
         game.load.spritesheet('ground', 'resources/art/platform.png', 123, 204);
 
@@ -24,14 +24,23 @@ demo.state1.prototype = {
 
         // Generating player 1
         player = game.add.sprite(400, 100, 'tester');
-        //game.physics.enable([player], Phaser.Physics.ARCADE);
-        //player.anchor.setTo(0.5,0.5);
-        //player.enableBody = true;
+
 
 
         //selects frames from the assigned spritesheet and sets them apart for its animation
         player.animations.add('idle',[0,1,2,3,4,5,6,7], 12, true);
         player.animations.add('run', [8,9,10,11,12,13,14,15], 12, true);
+        player.animations.add('jump', [16,17,18,19,20,21,22,23,24,25], 12, false);
+        //neutralpunch2 would follow nuetralpunch1 after it finishes running, like a combo
+        //would require input, let's say that hitting 'a' for example, would trigger neutralPunch1, if pressed again at the right...
+        //..moment, would trigger neutralPunch2, and so forth 
+        player.animations.add('neutralPunch1', [28,29,30,31], 12, false);
+        player.animations.add('neutralPunch2', [32,33,34,35], 12, false);
+        player.animations.add('neutralPunch3', [36,37,38], 12, false);
+        player.animations.add('neutralPunch3', [36,37,38], 12, false);
+        player.animations.add('neutralPunch4', [39,40], 12, false);
+        player.animations.add('neutralPunch5', [41,42,43,44], 12, false);
+        player.animations.add('neutralKick', [45,46,47,48,49,50,51], 12, false);
 
         //plays added animaiton
         player.animations.play('idle');
@@ -43,47 +52,90 @@ demo.state1.prototype = {
         game.physics.arcade.enable([player, platform]);
         player.body.collideWorldBounds = true;
         platform.enableBody = true;
-        player.body.gravity.y = 1500;
+        player.body.gravity.y = 1900;
         platform.body.immovable = true;
-
-        /*
-        sprite2 = game.add.sprite(300,700,'ground');
-            game.physics.arcade.enable(sprite2);
-            sprite2.body.allowGravity = false;
-            sprite2.body.enableBody = true;
-            sprite2.body.immovable = true;
-        */
+        
     },
     update: function () {
         //player and platform will collide
         game.physics.arcade.collide(player, platform);
-        //plays 'run' animation on keyDown RIGHT and LEFT
+    
+
+
+
+        //when the left arrow key is held down
         if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-        
             player.animations.play('run', 13, true);
             //first arg in setTo flips sprite orientation to LEFT
             player.scale.setTo(-1, 1);
             player.x -= 8;
+
+            if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+                //player.animations.stop('run');
+                player.animations.play('jump');
+                player.y -= 14;
+    
+            }
+
+
         }
+
+        //when the right arrow key is held down
         else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
             //first arg in setTo flips sprite orientation to RIGHT
             player.scale.setTo(1, 1);
             player.animations.play('run', 13, true);
             player.x += 8;
+
+            //If RIGHT arrow is currently being pressed and UP just got pressed...
+            //Needs work,'jump' animation is not playing...
+            if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+                //player.animations.stop('run');
+                player.animations.play('jump');
+                player.y -= 14;
+    
+            }
+
+
+            //when the up arrow key is held down
+        }else if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+            player.animations.play('jump');
+            player.y -= 14;
+
         } else {
             //Playes 'idle' animation if no LEFT or RIGHT keys are pressed
-            player.animations.stop('run');
+            //player.animations.stop('run');
             player.animations.play('idle', 13, true);
 
         }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         //changes player position onkeydown
+
+        /*
         if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-            player.y -= 7;
+            player.animations.stop('run');
+            player.animations.stop('idle');
+            player.animations.play('jump');
+            player.y -= 14;
         }
         else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
             player.y += 2;
         }
+        */
 
 
         /*
@@ -94,6 +146,17 @@ demo.state1.prototype = {
         */
     }
 };
+
+
+
+
+
+
+
+
+
+
+
 
 // function players () {
 //     let player = game.add.sprite(492,408, 'test');
