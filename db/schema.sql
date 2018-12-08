f6140e6bc4daa80ad9b2d73f653daf05344258ed
David Baker <daviiddamiianbaker@gmail.com>
2018-12-08 15:11:59 -0500

    modifying db

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,8 @@ USE user_ranksDB;
 
 CREATE TABLE ranks (
     id NOT NULL AUTO_INCREMENT,
-    high_score INT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    attempts INT NOT NULL,
     hits INT NOT NULL,
     PRIMARY KEY (id)
 )
