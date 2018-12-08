277a4430f65a8c70bc9762e7ba57b8e3aa4faa3f
David Baker <daviiddamiianbaker@gmail.com>
2018-12-08 03:02:17 -0500

    wip

--- public/client/js/main.js
+++ public/client/js/main.js
@@ -1,3 +1,5 @@
-let game = new Phaser.Game(1000,800,Phaser.AUTO, document.getElementById('game'));
-game.state.add('state1', demo.state1);
-game.state.start('state1');
+let game = new Phaser.Game(1200,1000,Phaser.AUTO, document.getElementById('game'));
+game.state.add('state0', demo.state0);
+// game.state.add('state1', demo.state1);
+game.state.start('state0');
+//game.state.start('state1');
