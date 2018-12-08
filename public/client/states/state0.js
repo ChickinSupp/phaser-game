b9be198677d5772bac04095932f33c6bbe095208
David Baker <daviiddamiianbaker@gmail.com>
2018-12-08 15:16:23 -0500

    working on starting state

--- public/client/states/state0.js
+++ public/client/states/state0.js
@@ -1,26 +1,54 @@
 let demo = window.demo || (window.demo = {});
-
-//var demo = {};
 let background;
+
+// Loading start sprites and animation
+// function loadStartSprite () {
+//     background = game.add.sprite(0,0,'startMenu');
+//     background.height = 800;
+//     background.width = 1000;
+    
+//     background.animations.add('start', [0,1,2] , 3, false);
+//     background.animations.add('enter', [2,3] , 3, true);
+//     background.animations.play('start');
+// };
+
+// function Menu (){
+//     var self = this;
+//     this.gameRoom;
+//     this.viewId;
+//     this.playerCount;
+//     this.selectchar;
+//   }
+
 demo.state0 = function () {};
 demo.state0.prototype = {
-    preload: function () {
+    init: function () {
+        this.titleText = game.make.text(game.world.centerX, 90, 'Pixel Smash', {
+        font: '100px PipeDream',
+        align: 'center',
+    });
+    this.titleText.addColor('white', 0);
+    this.titleText.addColor('white', 1);
+    this.titleText.addColor('white', 2);
+    this.titleText.addColor('white', 3);
+    this.titleText.addColor('white', 4);
+    this.titleText.addColor('white', 5);
+    this.titleText.addColor('white', 7);
+    this.titleText.addColor('white', 8);
+    this.titleText.addColor('white', 9);
+    this.titleText.addColor('white', 10);
 
-        game.load.spritesheet('startMenu', 'client/assets/art/start.png',600 ,600 , 4);
+    this.titleText.anchor.setTo(0.5);
+    this.titleText.setShadow(5, 5, 'rgba(0, 0, 0, 0.5)', 0);
     },
     create: function () {
-background = game.add.sprite(0,0,'startMenu');
-background.height = 800;
-background.width = 1000;
-background.animations.add('start-state', [0,1,], 3, false);
-
-background.animations.add('enter', [2,3], 3,true);
-
-background.animations.play('start');
-
-
+        game.add.existing(this.titleText);
     },
     update: function () {
-        background.animations.play('enter');
-    },
+        // if(background.animations.currentAnim.name =='start' && background.animations.currentAnim.isFinished) {
+
+        //     background.animations.play('enter');
+        // }
+        //background.animations.play('enter');
+    }
 };
